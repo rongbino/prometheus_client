1308a7dccfd9162a5e317ff4360483f419ad9bb3
SOGOU-INC\rongbin <rongbin@sogou-inc.com>
2018-04-17 14:28:04 +0800

    init prometheus client for py

---
+++ py/py_client.py
@@ -0,0 +1,19 @@
+from prometheus_client import start_http_server, Summary
+import random
+import time
+
+# create a metrix to track time spent and requests made
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+# Decorate functionwith metric
+@REQUEST_TIME.time()
+def process_request(t):
+    """ A dump function that takes some time."""
+    time.sleep(t)
+
+if __name__ == '__main__':
+    # Start up the server to expose the metrics
+    start_http_server(9090)
+    # Genrrate some requests
+    while True:
+        process_request(random.random())
